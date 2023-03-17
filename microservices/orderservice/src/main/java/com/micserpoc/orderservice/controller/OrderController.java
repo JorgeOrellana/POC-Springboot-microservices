ee557402efb7fe3c45138fd07178c85c912e14d6
Jorge <coco_orellana@hotmail.com>
2023-03-16 21:44:24 -0400

    implement circui brakear

--- microservices/orderservice/src/main/java/com/micserpoc/orderservice/controller/OrderController.java
+++ microservices/orderservice/src/main/java/com/micserpoc/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.micserpoc.orderservice.controller;
 
 import com.micserpoc.orderservice.dto.OrderRequestDTO;
 import com.micserpoc.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +15,15 @@ public class OrderController
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequestDTO orderRequest)
     {
         orderService.placeOrder(orderRequest);
         return "Order placed successfully";
+    }
+
+    public String fallbackMethod(OrderRequestDTO orderRequestDTO, RuntimeException runtimeException)
+    {
+        return "Oops! Sometime went wrong, please order after some time!";
     }
 }
